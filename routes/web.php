f06bb6e9d8ee864b163ab9db15df0dbdbab097ab
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-07 15:33:00 -0600

    :sparkles: Sección 8

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('post', PostController::class);
-
-Route::resource('category', CategoryController::class);
+Route::resource([
+    'post' => PostController::class,
+    'category' => CategoryController::class
+]);
 
 // Route::get('post', [PostController::class, 'index']);
 // Route::get('post/{post}', [PostController::class, 'show']);
@@ -31,5 +32,3 @@ Route::resource('category', CategoryController::class);
 // Route::post('post', [PostController::class, 'store']);
 // Route::put('post/{post}', [PostController::class, 'update']);
 // Route::delete('post/{post}', [PostController::class, 'delete']);
-
-
