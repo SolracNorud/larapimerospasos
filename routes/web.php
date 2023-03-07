3002a9d36272fa82f51c03347caa08072bc8f696
Solrac Norud <carlos.duron@cceo.com.mx>
2023-03-07 13:59:04 -0600

    :sparkles: Sección 6

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\Dashboard\PostController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [TestController::class,'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource('post', PostController::class);
+
+// Route::get('post', [PostController::class, 'index']);
+// Route::get('post/{post}', [PostController::class, 'show']);
+// Route::get('post/create', [PostController::class, 'create']);
+// Route::get('post/{post}/edit', [PostController::class, 'edit']);
+// Route::post('post', [PostController::class, 'store']);
+// Route::put('post/{post}', [PostController::class, 'update']);
+// Route::delete('post/{post}', [PostController::class, 'delete']);
+
+
